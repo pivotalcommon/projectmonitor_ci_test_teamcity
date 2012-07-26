d6a02b0eca94d7ee09cff9c40b3d3cfdcdb198c0
Pivotal Labs Common Effort Role Account <accounts+github+common@pivotallabs.com>
2012-07-26 14:43:12 -0300

    only sleep for 2 minutes

--- spec/sleepy_spec.rb
+++ spec/sleepy_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe "sleeping" do
-  it "sleeps for 15 minutes so that we can see specs running" do
+  it "sleeps for 2 minutes so that we can see specs running" do
     expect {
-      sleep( 15 * 60 )
+      sleep( 2 * 60 )
     }.to change{ Time.now }
   end
 end
